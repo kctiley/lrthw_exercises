ca31e1be83599af91ea999291a0e1c7f267f5d4e
Klint Tiley <kctiley@gmail.com>
2015-02-12 22:27:10 -0700

    added comments

---
+++ chapter_3/Readme.md
@@ -0,0 +1,9 @@
++ adds
+- subtracts
+/ divides
+* multiplies
+% divides and returns only the remainder
+< compares something as less than something else
+> compares something as greater than something else
+<= compares something as less than or equal to something else
+>= compares something as greater than or equal to something else
--- chapter_3/ex3.rb
+++ chapter_3/ex3.rb
@@ -1,23 +1,41 @@
-puts "I will count my chickens:"
+# prints string
+puts 'I will count my chickens:'
 
+# prints string and string of 25 added to result of 30 divided by 6
 puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 -25 * 3 % 4}"
 
-puts "Now I will count the eggs:"
+# prints string and string of 100 minus the remainder of 25 times 3 divided by 4
+puts "Roosters #{100.0 -25 * 3 % 4}"
 
-puts 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
+# puts string
+puts 'Now I will count the eggs:'
 
-puts "Is it true that 3 + 2 < 5 -7?"
+# puts result of this math using PEMDAS
+puts 3.00 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
 
+# prints string
+puts 'Is it true that 3 + 2 < 5 -7?'
+
+# prints true boolean
 puts 3 + 2 < 5 - 7
 
+# prints string and 3 + 2 result as part of the string
 puts "What is 3 + 2? #{3 + 2}"
+
+# prints string and 5 - 7 result as part of the string
 puts "What is 5 - 7? #{5 - 7}"
 
+# prints string
 puts "Oh, that's why it's false."
 
-puts "How about some more."
+# prints string
+puts 'How about some more.'
 
+# prints string and boolean as part of the string
 puts "Is it greater? #{5 > -2}"
+
+# similar to previous
 puts "Is it greater or equal? #{5 >= -2}"
+
+# similar to previous
 puts "Is it less or equal? #{5 <= -2}"
