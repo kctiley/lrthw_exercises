0f80e4a3514d1243ce7b1bfbcd1f3141bc80c607
Klint Tiley <kctiley@gmail.com>
2015-03-28 14:51:34 -0600

    added triple quote examples

--- chapter_10/ex10.rb
+++ chapter_10/ex10.rb
@@ -13,3 +13,22 @@ puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat
+puts ''
+
+puts '''word1
+word2 '''
+puts ''
+
+puts """word 3
+word 4"""
+puts ''
+
+puts '''This should not \nhave more than one line'''
+puts ''
+
+puts """This should have more than\n one line"""
+puts ''
+
+puts """This should also\nhave more
+than one line
+maybe even four lines"""
